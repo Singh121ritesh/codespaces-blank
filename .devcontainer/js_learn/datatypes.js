6709e13190f704808b5be532eca435b28f9b241e
Ritesh Singh <141363371+Singh121ritesh@users.noreply.github.com>
2023-11-14 11:59:00 +0000

    variable.js

---
+++ .devcontainer/js_learn/datatypes.js
@@ -0,0 +1,8 @@
+const accountid = 1234 ;
+var accountname = "Union Bnak of india";
+let accountgmail="Singhrttu123goolgle.com"
+
+//accountid=123; const ke bad agar ussi variable me kuchh aur assign karoge to maroge node.js execute hi nahi hoga
+accountgmail="Shambhunath123@google.com";
+accountname="Stat bank of india";
+console.table([accountgmail,accountid,accountname]);
